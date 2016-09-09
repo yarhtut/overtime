99a3244e01ab02463b52d31c4ac98794aefddfaf
Yar Thet Pan Kyaw Htut <yar@boost.co.nz>
2016-09-10 09:25:01 +1200

    add fullname method

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,8 @@ class User < ApplicationRecord
 
 
   validates_presence_of :first_name, :last_name
+
+  def full_name
+    last_name.upcase + ", " + first_name.upcase
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,15 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  before do
+    @user = User.create(email: 'test@test.com',
+                        password: '123123123',
+                        password_confirmation: '123123123',
+                        first_name: 'john',
+                        last_name: 'doe')
+  end
   describe 'creation' do
-    before do
-      @user = User.create(email: 'test@test.com',
-                          password: '123123123',
-                          password_confirmation: '123123123',
-                          first_name: 'john',
-                          last_name: 'doe')
-    end
-
     it 'can be created' do
       expect(@user).to be_valid
     end
@@ -19,6 +18,11 @@ RSpec.describe User, type: :model do
       @user.last_name = nil
       expect(@user).to_not be_valid
     end
-    
+  end
+
+  describe 'custom name mathod' do
+    it 'can have full name' do
+      (@user.full_name).to be("DOE", "JOHN")
+    end
   end
 end
