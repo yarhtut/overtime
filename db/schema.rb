ff24eebe2d6c6617015bc9c3e72debff8f9b76cd
Yar Thet Pan Kyaw Htut <yar@boost.co.nz>
2016-09-16 07:46:47 +1200

    add enum

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160909020803) do
+ActiveRecord::Schema.define(version: 20160915193245) do
 
   create_table "posts", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.date     "date"
     t.text     "rationale",  limit: 65535
-    t.datetime "created_at",               null: false
-    t.datetime "updated_at",               null: false
+    t.datetime "created_at",                           null: false
+    t.datetime "updated_at",                           null: false
     t.integer  "user_id"
+    t.integer  "status",                   default: 0
     t.index ["user_id"], name: "index_posts_on_user_id", using: :btree
   end
 
