0c2cc4dc28ca47d2807ffe7ee5e4b92b00085d3e
Yar Thet Pan Kyaw Htut <yar@boost.co.nz>
2016-09-18 15:17:11 +1200

    add policy for edit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,10 @@
 class ApplicationController < ActionController::Base
+  include Pundit
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+  
   def resource_params
-  params.require(:user).permit(dashboard.permitted_attributes)
-end
+    params.require(:user).permit(dashboard.permitted_attributes)
+  end
 
 end
---
+++ app/policies/post_policy.rb
@@ -0,0 +1,4 @@
+class PostPolicy < ApplicationPolicy
+  def update?
+    record.user_id == user.id || admin_types.include?(user.type
+end
