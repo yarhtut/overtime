50566242da8c4477275d2d412ee360f5bc783a9b
Yar Thet Pan Kyaw Htut <yar@boost.co.nz>
2016-09-21 23:28:34 +1200

    add Auditlog resources and references to user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160915193245) do
+ActiveRecord::Schema.define(version: 20160921112414) do
+
+  create_table "audit_logs", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer  "user_id"
+    t.integer  "status",     default: 0
+    t.date     "start_date"
+    t.date     "end_date"
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+    t.index ["user_id"], name: "index_audit_logs_on_user_id", using: :btree
+  end
 
   create_table "posts", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.date     "date"
@@ -42,5 +52,6 @@ ActiveRecord::Schema.define(version: 20160915193245) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "audit_logs", "users"
   add_foreign_key "posts", "users"
 end
---
+++ spec/factories/audit_logs.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :audit_log do
+    user nil
+    status 1
+    start_date "2016-09-21"
+    end_date "2016-09-21"
+  end
+end
